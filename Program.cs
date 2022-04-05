dad30ab216811f35d54427fefdcd42fd4501c895
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-04-05 13:09:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SystemIO4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"c:\");
+
+            Console.WriteLine("\n Poddirektorij: ");
+
+            foreach (DirectoryInfo diInfo in di.GetDirectories())
+            {
+                Console.WriteLine("{0}\t\t\t{1}\t{2}", diInfo.Name, diInfo.CreationTime, diInfo.LastAccessTime);
+            }
+
+            foreach (FileInfo diInfo in di.GetFiles())
+            {
+                Console.WriteLine("{0}\t\t\t{1}\t{2}", diInfo.Name, diInfo.CreationTime, diInfo.LastAccessTime);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
